6437932f2c989b5df0148faea8ac062689e9369e
water127 <1561513500@qq.com>
2021-10-10 21:03:27 +0800

    :bulb: P60

--- 19_单文件组件/School.vue
+++ 19_单文件组件/School.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="demo">
-    <h2>学校名称: {{ schoolName }} </h2>
+    <h2>学校名称: {{ name }} </h2>
     <h2>学校地址: {{ address }} </h2>
     <button @click="showName">点我提示学校名</button> 
   </div>
@@ -10,7 +10,7 @@
   const School = Vue.extend({
     data() {
         return {
-            schoolName: 'hdu',
+            name: 'hdu',
             address: 'HZ'
         }
     },
---
+++ 19_单文件组件/Student.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="demo">
+    <h2>学生姓名: {{ name }} </h2>
+    <h2>学生年龄: {{ age }} </h2>
+    <button @click="showName">点我提示学校名</button> 
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Student',
+    data() {
+      return {
+        name: '张三',
+        age: 18
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
