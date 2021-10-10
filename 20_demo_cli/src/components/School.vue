b882893caac202acc425334221136f71bd0dab2a
water127 <1561513500@qq.com>
2021-10-11 00:28:33 +0800

    :bulb: P63

---
+++ 20_demo_cli/src/components/School.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="demo">
+    <h2>学校名称: {{ name }} </h2>
+    <h2>学校地址: {{ address }} </h2>
+    <button @click="showName">点我提示学校名</button> 
+  </div>
+</template>
+
+<script>
+  const School = {
+    data() {
+        return {
+            name: 'hdu',
+            address: 'HZ'
+        }
+    },
+    methods: {
+        showName() {
+            alert(this.schoolName)
+        }
+    },
+  }
+  export default School;
+</script>
+
+<style>
+
+</style>
--- 20_demo_cli/src/main.js
+++ 20_demo_cli/src/main.js
@@ -4,5 +4,11 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+})
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App),
+// }).$mount('#app')
