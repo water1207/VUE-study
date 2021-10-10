abb49c10ee7a1951f487e591045f501de4ac8ec6
water127 <1561513500@qq.com>
2021-10-10 20:59:36 +0800

    :bulb: P60

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="demo">
+    <h2>学校名称: {{ schoolName }} </h2>
+    <h2>学校地址: {{ address }} </h2>
+    <button @click="showName">点我提示学校名</button> 
+  </div>
+</template>
+
+<script>
+  const School = Vue.extend({
+    data() {
+        return {
+            schoolName: 'hdu',
+            address: 'HZ'
+        }
+    },
+    methods: {
+        showName() {
+            alert(this.schoolName)
+        }
+    },
+  })
+  export default school;
+</script>
+
+<style>
+
+</style>
