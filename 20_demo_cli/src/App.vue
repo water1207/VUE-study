d002aee212418aeccd0b309f6b4d012b392c95d5
water127 <1561513500@qq.com>
2021-11-01 15:14:42 +0800

    :fire: P106

--- 20_demo_cli/src/App.vue
+++ 20_demo_cli/src/App.vue
@@ -1,33 +1,18 @@
 <template>
   <div>
-    <!--具名插槽-->
-    <Category title="美食" :listData="foods">
-      <template slot="demo">
-        <img src="./assets/logo.png" alt="">
-      </template>
-
-    </Category>
-    <!--作用域插槽-->
-    <C2>
-      <template v-slot:default="{user}">
-        {{user.a}}
-      </template>
-    </C2>
+    <Count></Count>
   </div>
 </template>
 
 <script>
-  import Category from "./components/Category";
-  import C2 from "./components/C2";
+  import Count from "./components/Count";
 
   export default {
     name: 'App',
-    components: {Category, C2},
+    components: {Count},
     data() {
       return {
-        foods: ['火锅', '烤肉', '小龙虾', '牛排'],
-        games: ['地平线4', '红色警戒', '超级玛丽'],
-        film: ['不能说的秘密', '天台爱情故事', '头文字D']
+
       }
     },
     methods: {
---
+++ 20_demo_cli/src/components/Count.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>当前求和为: {{$store.state.sum}}</h1>
+    <h2>当前name为: {{name}}</h2>
+    <h2>当前sex为: {{sex}}</h2>
+<!--    <h2>当前求和放大10倍为: {{$store.getters.bigSum}}</h2>-->
+    <h2>当前求和放大10倍为: {{$store.getters.bigSum}}</h2>
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="increment(n)">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">奇数+</button>
+    <button @click="incrementWait">wait +</button>
+  </div>
+</template>
+
+<script>
+  import {mapState, mapGetters, mapMutations, mapActions} from 'vuex'
+
+  export default {
+    name: "Count",
+    data() {
+      return {
+        n: 1,
+      }
+    },
+    computed: {
+      //借助mapState生成计算属性，从state中读取数据(对象写法)
+      //...mapState({name:'name', sex: 'sex'}),
+      //数组写法
+      ...mapState(['name', 'sex']),
+      ...mapGetters(['bigSum'])
+    },
+    methods: {
+      ...mapActions({increment: 'add'}),
+      // ...mapMutations({increment: 'ADD'}),
+      // increment() {
+      //   this.$store.commit('ADD', this.n)  //没有特殊业务逻辑 直接略过action
+      //   //this.$store.dispatch('add', this.n)
+      // },
+      decrement() {
+
+      },
+      incrementOdd() {
+
+      },
+      incrementWait() {
+        setTimeout(() => {
+
+        },500)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
